affb069115f8acc1a9fd355854cbf87d0569b357
Smulemun <45842070+Smulemun@users.noreply.github.com>
2019-04-18 16:39:10 +0600

    Add files via upload

---
+++ addnewsform.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+ 
+class AddNewsForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Text', validators=[DataRequired()])
+    submit = SubmitField('Add')
